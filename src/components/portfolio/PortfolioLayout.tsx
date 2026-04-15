d55aae068fa0d3dd4b1e9eb0a2428bc74b6936e8
chowdhuryrz <rmc.2917@gmail.com>
2026-04-15 00:52:23 -0400

    revert: remove hex background, restore plain background

--- src/components/portfolio/PortfolioLayout.tsx
+++ src/components/portfolio/PortfolioLayout.tsx
@@ -1,13 +1,11 @@
 import { Hero } from "./Hero";
 import { RightContent } from "./RightContent";
-import { HexBackground } from "./HexBackground";
 import ErrorBoundary from "@/components/ErrorBoundary";
 
 export const PortfolioLayout = () => {
   return (
     <ErrorBoundary>
       <div className="min-h-screen">
-        <HexBackground />
         <div className="max-w-3xl mx-auto px-6 md:px-8">
           <Hero />
           <RightContent />
